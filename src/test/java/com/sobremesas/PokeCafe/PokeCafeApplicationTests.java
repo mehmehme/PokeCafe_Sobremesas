61f59fa815c165a16bcfa19a7eaae0fa7325b190
LisinhaDeGranaaaa <83306150+mehmehme@users.noreply.github.com>
2025-06-03 17:59:28 -0300

    first commit

---
+++ src/test/java/com/sobremesas/PokeCafe/PokeCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobremesas.PokeCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
